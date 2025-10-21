b3fa773c57ad130b7511f4cc765f6af668887a80
Maria <girlfrombaikal@gmail.com>
2025-10-22 02:40:13 +0300

    version without security

---
+++ src/main/java/org/example/APIGatewayServiceApplication.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class APIGatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("eureka_route", r -> r
+                        .path("/api/bookings/**")
+                        .uri("lb://BOOKING-SERVICE"))
+                .route("eureka_route", r -> r
+                        .path("/api/hotels/**")
+                        .uri("lb://HOTEL-SERVICE"))
+                .build();
+    }
+}
